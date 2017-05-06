2ace39e9131ee3eb50d9e079292bb79c6d6f0816
Duje Medak <duje.medak95gmail.com>
2017-05-06 19:29:56 +0200

    updated game and gameScreen

--- app/src/main/java/com/fer/ppij/what/Game.java
+++ app/src/main/java/com/fer/ppij/what/Game.java
@@ -1,5 +1,6 @@
 package com.fer.ppij.what;
 
+import com.fer.ppij.what.database.QuestionDAL;
 import com.fer.ppij.what.database.model.AbstractQuestion;
 
 import java.util.List;
@@ -10,22 +11,24 @@ import java.util.List;
 //za pamcenje potrebnih aspekata igre(kategorija,score,nick,...)
 public class Game {
 
-    private int score;
+    private int score = 0;
     private String category;
     private String nick;
-    private int currentQuestion;
+    private int currentQuestionNumber = 0;
     private List<AbstractQuestion> questionPool;
+    private int lives;
+    boolean finished = false;
 
     public List<AbstractQuestion> getQuestionPool() {
         return questionPool;
     }
 
-    public Game(String category, String nick) {
+    public Game(String category, String nick, int wrongAnswers) {
         this.category = category;
         this.nick = nick;
-        score = 0;
-        currentQuestion = 0;
-
+        //TODO callback
+        this.questionPool = QuestionDAL.getQuestions(category);
+        this.lives = wrongAnswers;
     }
 
 
@@ -41,13 +44,26 @@ public class Game {
         return nick;
     }
 
-    public int getCurrentQuestion() {
-        return currentQuestion;
+    public boolean isFinished() {
+        return finished;
     }
 
-    public void nextQuestion(){
-        currentQuestion +=1;
+    public  AbstractQuestion getNextQuestion(){
+        int curr = currentQuestionNumber;
+        currentQuestionNumber +=1;
         score+=1;
+        if(curr < questionPool.size()){
+            return questionPool.get(currentQuestionNumber);
+        }
+        return  null;
     }
+
+    public void decreaseNumberOfLives(){
+        lives -= 1;
+        if(lives == 0){
+            finished = true;
+        }
+    }
+
 
 }
--- app/src/main/java/com/fer/ppij/what/GameScreen.java
+++ app/src/main/java/com/fer/ppij/what/GameScreen.java
@@ -16,6 +16,9 @@ import com.fer.ppij.what.database.model.ImageFillInQuestion;
 import com.fer.ppij.what.database.model.ImageMultipleChoiceQuestion;
 import com.fer.ppij.what.database.model.MultipleChoiceQuestion;
 
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by Mateo on 5/2/2017.
  */
@@ -23,18 +26,25 @@ import com.fer.ppij.what.database.model.MultipleChoiceQuestion;
 public class GameScreen extends AppCompatActivity {
 
     private TextView questionDisplayTextView;
-    private Button answerA, answerB, answerC, answerD;
+    private Button answerA, answerB, answerC, answerD, checkAnswerButton;
 
     private  ImageView questionImage;
     private  LinearLayout multipleAnswerLayout;
     private  LinearLayout fillAnswerLayout;
     private EditText fillEditText;
 
+    private String nickname,gameName;
+    Game game;
+    AbstractQuestion currentQuestion;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        nickname = getIntent().getStringExtra("nickname");
+        gameName = getIntent().getStringExtra("gameName");
 
+        game = new Game(gameName,nickname,3);
 
         questionImage = (ImageView)findViewById(R.id.questImage);
         multipleAnswerLayout = (LinearLayout)findViewById(R.id.fourAnswerLayout);
@@ -46,23 +56,21 @@ public class GameScreen extends AppCompatActivity {
         answerB = (Button) findViewById(R.id.answerB);
         answerC = (Button) findViewById(R.id.answerC);
         answerD = (Button) findViewById(R.id.answerD);
+        checkAnswerButton = (Button)findViewById(R.id.check_answer_button);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        Question question = new Question("Koje je pitanje?", "Odgovor", "Odgovor drugi", "Odgovor treci", "Odgovor cetvrti");
+        //Question question = new Question("Koje je pitanje?", "Odgovor", "Odgovor drugi", "Odgovor treci", "Odgovor cetvrti");
 
-        questionDisplayTextView.setText(question.getQuestionText());
-        answerA.setText(question.getCorrectAnswer());
-        answerB.setText(question.getIncorrectAnswer1());
-        answerC.setText(question.getIncorrectAnswer2());
-        answerD.setText(question.getIncorrectAnswer3());
+        displayNextQuestion();
 
 
 
-        answerA.setOnClickListener(new View.OnClickListener() {
+
+      /*  answerA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(GameScreen.this, EndScreen.class);
@@ -70,23 +78,78 @@ public class GameScreen extends AppCompatActivity {
                 finish();
             }
         });
-
+*/
 
     }
 
-    public void displayQuestion(AbstractQuestion question){
+    public void displayMultipleQuestion(MultipleChoiceQuestion question){
+        List<String > answers = question.getAnswers();
+        int a,b,c,d=0;
+        Random rnd = new Random();
+
+        a = rnd.nextInt(4);
+
+        b=rnd.nextInt(4);
+        while (b == a){
+            b = rnd.nextInt(4);
+        }
+
+        c=rnd.nextInt(4);
+        while (c == a || c == b){
+            c=rnd.nextInt(4);
+        }
+
+        for(int i=0;i<4;i++){
+            if(a != i && b!= i && c!= i){
+                d=i;
+                return;
+            }
+        }
+
+        questionDisplayTextView.setText(question.getText());
+        answerA.setText(answers.get(a));
+        answerB.setText(answers.get(b));
+        answerC.setText(answers.get(c));
+        answerD.setText(answers.get(d));
+    }
+
+    public void displayMultipleChoiceWithImage(ImageMultipleChoiceQuestion question){
+        displayMultipleQuestion(question);
+        //TODO namistit izvor slike
+        //questionImage.setBackground(question.getImage());
+    }
+
+    public void displayFillInQuestion(FillInQuestion question){
+        questionDisplayTextView.setText(question.getText());
+    }
+
+    public void displayFillInQuestionWithImage(ImageFillInQuestion question){
+        displayFillInQuestion(question);
+        //TODO namistit izvor slike
+        //questionImage.setImageBitmap();
+    }
+
+    public void displayNextQuestion(){
+        AbstractQuestion question = game.getNextQuestion();
+        if(question == null) return;
+
+        currentQuestion = question;
 
         if(question instanceof ImageMultipleChoiceQuestion){
             setDisplayVisibility(View.VISIBLE,View.VISIBLE,View.GONE);
+            displayFillInQuestionWithImage((ImageFillInQuestion) question);
         }
         else if(question instanceof MultipleChoiceQuestion){
             setDisplayVisibility(View.GONE,View.VISIBLE,View.GONE);
+            displayMultipleQuestion((MultipleChoiceQuestion)question);
         }
         else if(question instanceof ImageFillInQuestion){
             setDisplayVisibility(View.VISIBLE,View.GONE,View.VISIBLE);
+            displayFillInQuestionWithImage((ImageFillInQuestion)question);
         }
         else if(question instanceof FillInQuestion){
             setDisplayVisibility(View.GONE,View.GONE,View.VISIBLE);
+            displayFillInQuestion((FillInQuestion)question);
         }
 
     }
@@ -98,5 +161,75 @@ public class GameScreen extends AppCompatActivity {
     }
 
 
+    public void LoadEndScreen(){
+        Intent intent = new Intent(this, EndScreen.class);
+        intent.putExtra("nickname", nickname);
+        intent.putExtra("gameName", gameName);
+        intent.putExtra("score",game.getScore());
+        startActivity(intent);
+    }
 
+    public boolean checkAnswer(String answer){
+        String correctAnswer = currentQuestion.getCorrectAnswer();
+        if(correctAnswer.equals(answer)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public Button getCorrencAnswerButton(){
+        if(checkAnswer(answerA.getText().toString())){
+            return answerA;
+        }
+        else if(checkAnswer(answerB.getText().toString())){
+            return answerB;
+        }
+        else if(checkAnswer(answerC.getText().toString())) {
+            return answerC;
+        }
+        else {
+            return answerD;
+        }
+    }
+
+    public void colorizeButtons(Button choosenButton){
+        if(choosenButton == getCorrencAnswerButton()){
+            //TODO obojaj ga u zeleno
+            return;
+        }
+        else {
+            //TODO jos bojanja
+            //obojaj ga u crveno
+            //getCorrencAnswerButton() obojaj u zeleno
+        }
+    }
+
+    public void OnAnswerAClick(View view) {
+        colorizeButtons(answerA);
+        if(checkAnswer(answerA.getText().toString())){
+            //TODO dodaj novi botun za dalje koji poziva displayNextQuestion()
+        }
+        else {
+            game.decreaseNumberOfLives();
+            if(game.isFinished()){
+                //pricekaj malo
+                LoadEndScreen();
+            }
+            else {
+                //dodaj botun za dalje
+            }
+        }
+    }
+
+    public void OnAnswerBClick(View view) {
+        //TODO iskopiraj prethodne botune
+    }
+
+    public void OnAnswerCClick(View view) {
+    }
+
+    public void OnAnswerDClick(View view) {
+    }
 }
