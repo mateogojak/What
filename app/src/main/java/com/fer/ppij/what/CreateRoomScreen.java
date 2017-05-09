511fad8bfaaf8e2fa9098d3dd182c33609cd8926
mateogojak <mateogojak333@gmail.com>
2017-05-09 12:52:31 +0200

    activity

---
+++ app/src/main/java/com/fer/ppij/what/CreateRoomScreen.java
@@ -0,0 +1,22 @@
+package com.fer.ppij.what;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mateo on 5/9/2017.
+ */
+
+public class CreateRoomScreen extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_room);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
